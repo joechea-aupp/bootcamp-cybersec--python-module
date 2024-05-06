a8165cf2a9eaead115d92e5fcc08271b2afec367
joechea <s.chea@aupp.edu.kh>
2024-05-06 16:11:43 +0700

    update epoch lesson - add epoch detail

--- 5_working_with_time.py
+++ 5_working_with_time.py
@@ -6,6 +6,10 @@ sleep(5)
 print("End")
 
 # epoch time
+# Epoch time, also known as Unix time or POSIX time, 
+# represents the number of seconds that have elapsed 
+# since the Unix epoch (January 1, 1970, at 00:00:00 UTC).
+
 print("Epoch time")
 epoch_time = time()
 print(epoch_time)
