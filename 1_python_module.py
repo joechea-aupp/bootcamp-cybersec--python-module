17bcdbbf782236061238a32dee59b587bf7bd16d
joechea <s.chea@aupp.edu.kh>
2024-05-06 18:04:20 +0700

    update file 1

--- 1_python_module.py
+++ 1_python_module.py
@@ -1,13 +1,10 @@
 import math
-import inspect
 
 print("square root of 16", math.sqrt(16))
 
 dec_number = 1.5
-print('round up number: ', math.ceil(dec_number))
-print('round down number: ', math.floor(dec_number))
-print('original number: ', dec_number)
+print("round up number: ", math.ceil(dec_number))
+print("round down number: ", math.floor(dec_number))
+print("original number: ", dec_number)
 
 # print(dir(math))
-
-print(inspect.getfile(math))
