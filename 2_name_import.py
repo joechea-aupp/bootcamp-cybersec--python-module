78056743529cc325f143209ac1d8d23e5d4a75fc
joechea <s.chea@aupp.edu.kh>
2024-05-06 13:38:43 +0700

    initial commit

---
+++ 2_name_import.py
@@ -0,0 +1,10 @@
+from math import sqrt, ceil, floor
+
+print("square root of 16", sqrt(16))
+
+dec_number = 1.5
+print('round up number: ', ceil(dec_number))
+print('round down number: ', floor(dec_number))
+print('original number: ', dec_number)
+
+# print(dir(math))
